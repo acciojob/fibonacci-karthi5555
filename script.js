32a05b97317c6a62b63d7367617208f888502d4c
karthi5555 <105117281+karthi5555@users.noreply.github.com>
2023-12-13 18:08:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+	if(num == 1){
+		return 0;
+	}else if(num == 2){
+		return 1;
+	}
+	num = num - 2;
+	let a = 0, b = 1, c = 0;
+	
+	while(num != 0){
+		c = a + b;
+		a = b;
+		b = c;
+		num--;
+	}
+	return c;
+	
 }
 
 module.exports = fibonacci;
